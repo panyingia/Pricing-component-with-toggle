eafe4e5b454e5f05c28dd523ae63b150e4db53d2
panyingia <panyingia@gmail.com>
2023-07-05 10:45:22 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggle = document.getElementById('toggle')
+const flex = document.getElementById('flex')
+toggle.addEventListener('change', (e) => {
+    flex.classList.toggle('show-monthly')
+})
